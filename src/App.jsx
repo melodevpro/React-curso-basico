4e7fb98a275a565d31a875cd93bc5a1633969101
Matthew Duarte <hello.codehipster@outlook.com>
2025-07-17 13:16:02 -0500

    introduccion a react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header"
 import Footer from "./Footer";
+import Food from "./Food";
 
 function App() {
   return (
     <> 
       <Header/>
+      <Food/>
       <Footer/>
     </>
   );
